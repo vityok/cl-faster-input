645cf39d66e922d7f809544559356dcbb5d9cd0f
vityok <vityok@localhost>
2013-06-03 17:44:09 +0300

    improved and added new benchmarks, format readme

---
+++ README.md
@@ -0,0 +1,12 @@
+`cl-faster-input` A project for exploring and improving large files
+input, parsing, and processing in the Common Lisp applications.
+
+Standard read-line function is being blamed often for being slow and
+inefficient. Lisp offers a number of alternatives for reading and
+processing text files and large logs.
+
+This project will explore them all.
+
+Check current benchmark results at [the project wiki.](https://bitbucket.org/vityok/cl-faster-input/wiki/Home)
+
+Any suggestions, corrections and ideas are welcome!
--- benchmark-read-line.lisp
+++ benchmark-read-line.lisp
@@ -1,7 +1,15 @@
+;;; benchmarking READ-LINE and alternatives
 
+;; Tests are doing essentially what unix command "wc -l" does: reading
+;; the file and counting number of lines in it. The implementation
+;; differ however in the amount of memory used and the time it takes
+;; to complete the task.
+;;
 ;; How to run with Clozure CL:
 ;;
 ;; lx86cl --load benchmark-read-line.lisp --eval '(run)' --eval '(quit)'
+
+;; sbcl --load benchmark-read-line.lisp --eval '(run)' --eval '(quit)'
 
 
 (defconstant +kb+ 1024)
@@ -9,6 +17,18 @@
 (defconstant +file-size+ (* 10 +kb+)
   "This will produce a file 10MB in size.")
 (defconstant +fname+ "data.tmp")
+
+;; -----------------------------------------------------------
+
+(defvar *standard-optimize-settings*
+  '(optimize
+    speed
+    (safety 0)
+    (space 0)
+    (debug 0)
+    (compilation-speed 0)
+    #+:lispworks (hcl:fixnum-safety 0))
+  "The standard optimize settings used by most declaration expressions.")
 
 ;; -----------------------------------------------------------
 
@@ -35,17 +55,21 @@ file open and close operations."
 
 (defun run-test-read-line ()
   "Count lines using standard READ-LINE."
+
+  (declare #.*standard-optimize-settings*)
+
   (with-open-file (if +fname+
 		      :direction :input)
 
     (loop :for line = (read-line if nil)
-       :while line
-       :count line)))
+       :while line :count line)))
 
 ;; -----------------------------------------------------------
 
 (defun run-test-read-sequence-string ()
   "Count lines by reading data in chunks into a string."
+
+  (declare #.*standard-optimize-settings*)
 
   (with-open-file (is +fname+
 		      :direction :input)
@@ -53,9 +77,12 @@ file open and close operations."
     (let ((line (make-string (* 4 +kb+)
 			     :initial-element #\?))
 	  (count 0))
-      
+      (declare (fixnum count))
+
       (loop :while (> (read-sequence line is) 0)
-	 :do (incf count (count #\Newline line :test #'char=)))
+	 :do (loop :for c :across line
+		:when (char= c #\Newline)
+		:do (incf count)))
       count)))
 
 ;; -----------------------------------------------------------
@@ -66,28 +93,85 @@ file open and close operations."
 Taken a que from Stackoverflow:
 http://stackoverflow.com/a/15813006"
 
+  (declare #.*standard-optimize-settings*)
+
   (with-open-file (input-stream +fname+
 				:direction :input
 				:element-type '(unsigned-byte 8))
-    (let ((buf (make-array 4096 :element-type (stream-element-type input-stream)))
+    (let ((buf (make-array #.(* 16 +kb+) :element-type (stream-element-type input-stream)))
 	  (count 0))
+
+      (declare (fixnum count)
+	       (type (array (unsigned-byte 8)) buf))
+
       (loop :for pos = (read-sequence buf input-stream)
-	 :while (plusp pos)
-	 :do (incf count (count #.(char-code #\Newline) buf :test #'=))))))
+	 :while (plusp pos) :do
+	 (loop :for c :across buf
+	    :when (= c #.(char-code #\Newline))
+	    :do (incf count)))
+      count)))
+
+;; -----------------------------------------------------------
+
+(defun run-test-read-sequence-by-char ()
+  "Read the file char by char."
+  (declare #.*standard-optimize-settings*)
+  (with-open-file (input-stream +fname+
+				:direction :input)
+    (let ((count 0))
+
+      (declare (fixnum count))
+
+      (loop :for c = (read-char input-stream nil)
+	 :while c
+	 :when (char= c #\Newline)
+	 :do (incf count))
+      count)))
+
+;; -----------------------------------------------------------
+
+(defun run-test-read-sequence-by-byte ()
+  "Read the file byte by byte."
+
+  (declare #.*standard-optimize-settings*)
+
+  (with-open-file (input-stream +fname+
+				:element-type 'unsigned-byte
+				:direction :input)
+    (let ((count 0))
+      
+      (declare (fixnum count))
+
+      (loop :for b :of-type unsigned-byte = (read-byte input-stream nil 0)
+	 :while (/= b 0)
+	 :when (= b #.(char-code #\Newline))
+	 :do (incf count))
+
+      count)))
 
 ;; -----------------------------------------------------------
 
 (defun run ()
   (prepare)
 
+  (format t "WARMING UP~%")
+  (dotimes (i 10) (run-test-read-line))
+
   (format t "READ-LINE~%")
   (time (dotimes (i 10) (run-test-read-line)))
 
-;;  (format t "READ-SEQUENCE~%")
-;;  (time (dotimes (i 10) (run-test-read-sequence)))
+  (format t "READ-SEQUENCE~%")
+  (time (dotimes (i 10) (run-test-read-sequence-string)))
 
   (format t "RUN-TEST-READ-SEQUENCE-BYTE-ARRAY~%")
   (time (dotimes (i 10) (run-test-read-sequence-byte-array)))
+
+  (format t "RUN-TEST-READ-SEQUENCE-BY-CHAR~%")
+  (time (dotimes (i 10) (RUN-TEST-READ-SEQUENCE-BY-CHAR)))
+
+  (format t "RUN-TEST-READ-SEQUENCE-BY-BYTE~%")
+  (time (dotimes (i 10) (RUN-TEST-READ-SEQUENCE-BY-BYTE)))
+
   )
 
 ;; EOF
